db8ca27c4013ca22cea9d721c75cd59e48cb674a
Gray Monk <graymonk89@yandex.ru>
2022-01-22 21:22:43 +0300

    Урок 1. Добваил кнопку. При нажатии виджет TextView становится мидимым или невимым.

--- app/src/main/java/ru/gb/android_lesson1/MainActivity.java
+++ app/src/main/java/ru/gb/android_lesson1/MainActivity.java
@@ -3,6 +3,10 @@ package ru.gb.android_lesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Создать проект со следующими пользовательскими элементами:
@@ -20,5 +24,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("mylogs","MainActivity onCreate()");
+
+
+        Button button = findViewById(R.id.btn_show);
+        TextView textView = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("mylogs","onClick");
+                if(textView.getVisibility() == View.VISIBLE){
+                    textView.setVisibility(View.INVISIBLE);
+                    Log.d("mylogs","TextView INVISIBLE");
+                } else {
+                    textView.setVisibility(View.VISIBLE);
+                    Log.d("mylogs","TextView VISIBLE");
+                }
+            }
+        });
     }
 }
